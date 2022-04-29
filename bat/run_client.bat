549f05f101137fb613fd8ed745f94d3394b5a33b
Robertudo <sayamosfero@gmail.com>
2022-04-29 16:28:34 -0500

    Add error message when try to open twice exe file

--- bat/run_client.bat
+++ bat/run_client.bat
@@ -1,5 +1,10 @@
 @echo off
 
-cd ..
-taskkill /IM Client_CCVDE.exe /F
-start Client_CCVDE.exe
+tasklist /fi "ImageName eq Client_CCVDE.exe" /fo csv 2>NUL | find /I "Client_CCVDE.exe">NUL
+
+if "%ERRORLEVEL%"=="0" (
+    start init-error.vbs
+) else (
+    cd ..
+    start Client_CCVDE.exe
+)
